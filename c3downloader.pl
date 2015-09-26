620fb016a602d79259269c4920b847d4fb612b66
vsTerminus <vsTerminus@vsTerminus.ca>
2015-09-25 23:41:01 -0500

    Set download amount back to 2000 and removed a debug print statement

--- c3downloader.pl
+++ c3downloader.pl
@@ -29,7 +29,7 @@ sub comparable
 
 
 # Vars
-my $song_count = 30; # Fetch this many songs on a single page. Basically, set this high enough that you get everything.
+my $song_count = 2000; # Fetch this many songs on a single page. Basically, set this high enough that you get everything.
 my $sort_by = "ReleasedOn";
 my $sort_direction = "DESC";
 my $url = "http://pksage.com/songlist/php/songlist.php?_dc=1443067770349&whichGame=rb&andor=&page=1&start=0&limit=$song_count&sort=%5B%7B%22property%22%3A%22$sort_by%22%2C%22direction%22%3A%22$sort_direction%22%7D%5D&filter=%5B%7B%22property%22%3A%22Source%22%2C%22value%22%3A%22Custom%20Songs%7Cis%22%7D%5D";
@@ -143,7 +143,6 @@ foreach my $entry ( @{$table->{'data'}} )
     }
     elsif ( comparable($released) <= comparable($last_update_time) and comparable($updated) <= comparable($last_update_time) )
     {
-        print comparable($released) . " <= " . comparable($last_update_time) . " AND " . comparable($updated) . " <= " . comparable($last_update_time) . "\n";
         print localtime(time) . ": Skipping '" . $entry->{'FullName'} . "' as it has not changed since last time.\n";
     }
     else
